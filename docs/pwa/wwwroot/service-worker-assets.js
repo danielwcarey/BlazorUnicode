6ccd6be6b17d93390b9c253e6c5bde8ccc12499d
Daniel Carey <whawke8@hotmail.com>
2020-05-25 12:20:45 -0500

    Added pwa.

---
+++ docs/pwa/wwwroot/service-worker-assets.js
@@ -0,0 +1,213 @@
+﻿self.assetsManifest = {
+  "assets": [
+    {
+      "hash": "sha256-BHNhq10T\/9aejlrFEhS\/NYeYS3Djwv\/pqg2xUjQCx\/s=",
+      "url": "css\/app.css"
+    },
+    {
+      "hash": "sha256-rldnE7wZYJj3Q43t5v8fg1ojKRwyt0Wtfm+224CacZs=",
+      "url": "css\/bootstrap\/bootstrap.min.css"
+    },
+    {
+      "hash": "sha256-xMZ0SaSBYZSHVjFdZTAT\/IjRExRIxSriWcJLcA9nkj0=",
+      "url": "css\/bootstrap\/bootstrap.min.css.map"
+    },
+    {
+      "hash": "sha256-W84EHg\/M5hfjXvvDNbE3LSeoSstl+0ZMZf+AqzQkqIU=",
+      "url": "css\/fonts\/CascadiaCode.woff2"
+    },
+    {
+      "hash": "sha256-k8jjxEAj4pNmZmAYb6BEKxbfj\/vAWEZ8WcPN5JjnXEE=",
+      "url": "css\/fonts\/CascadiaCodePL.woff2"
+    },
+    {
+      "hash": "sha256-20JGFssEgw7rSIoe4etioZBVP8SqhvKZGUgIPpiBEaI=",
+      "url": "css\/fonts\/CascadiaMono.woff2"
+    },
+    {
+      "hash": "sha256-O6Q\/omOW33AklYHmukN6XF30mJRbq2yykUfGazqpsi8=",
+      "url": "css\/fonts\/CascadiaMonoPL.woff2"
+    },
+    {
+      "hash": "sha256-tbqFHdMa9Ni5QtYTz2LHuPetIYA41BD\/r2j0qvUFsjw=",
+      "url": "css\/myApp.css"
+    },
+    {
+      "hash": "sha256-jA4J4h\/k76zVxbFKEaWwFKJccmO0voOQ1DbUW+5YNlI=",
+      "url": "css\/open-iconic\/FONT-LICENSE"
+    },
+    {
+      "hash": "sha256-BJ\/G+e+y7bQdrYkS2RBTyNfBHpA9IuGaPmf9htub5MQ=",
+      "url": "css\/open-iconic\/font\/css\/open-iconic-bootstrap.min.css"
+    },
+    {
+      "hash": "sha256-OK3poGPgzKI2NzNgP07XMbJa3Dz6USoUh\/chSkSvQpc=",
+      "url": "css\/open-iconic\/font\/fonts\/open-iconic.eot"
+    },
+    {
+      "hash": "sha256-sDUtuZAEzWZyv\/U1xl\/9D3ehyU69JE+FvAcu5HQ+\/a0=",
+      "url": "css\/open-iconic\/font\/fonts\/open-iconic.otf"
+    },
+    {
+      "hash": "sha256-+P1oQ5jPzOVJGC52E1oxGXIXxxCyMlqy6A9cNxGYzVk=",
+      "url": "css\/open-iconic\/font\/fonts\/open-iconic.svg"
+    },
+    {
+      "hash": "sha256-p+RP8CV3vRK1YbIkNzq3rPo1jyETPnR07ULb+HVYL8w=",
+      "url": "css\/open-iconic\/font\/fonts\/open-iconic.ttf"
+    },
+    {
+      "hash": "sha256-cZPqVlRJfSNW0KaQ4+UPOXZ\/v\/QzXlejRDwUNdZIofI=",
+      "url": "css\/open-iconic\/font\/fonts\/open-iconic.woff"
+    },
+    {
+      "hash": "sha256-aF5g\/izareSj02F3MPSoTGNbcMBl9nmZKDe04zjU\/ss=",
+      "url": "css\/open-iconic\/ICON-LICENSE"
+    },
+    {
+      "hash": "sha256-p\/oxU91iBE+uaDr3kYOyZPuulf4YcPAMNIz6PRA\/tb0=",
+      "url": "css\/open-iconic\/README.md"
+    },
+    {
+      "hash": "sha256-vb\/7v8itTTptAbWJFRBFjz029xcEIq9Oor7TIRpz6Ls=",
+      "url": "data\/UnicodeData.txt"
+    },
+    {
+      "hash": "sha256-qU+KhVPK6oQw3UyjzAHU4xjRmCj3TLZUU\/+39dni9E0=",
+      "url": "favicon.ico"
+    },
+    {
+      "hash": "sha256-IpHFcQvs03lh6W54zoeJRG1jW+VnGmwymzxLoXtKX7Y=",
+      "url": "icon-512.png"
+    },
+    {
+      "hash": "sha256-nILMeTWtMHuOx2VpxpPmS8ZTy09rbSk9lyCl+Af34rQ=",
+      "url": "index.html"
+    },
+    {
+      "hash": "sha256-LcLsWRe\/BL07WKctmMSKtH7\/Q8RMIa5uAPjONATd8o0=",
+      "url": "manifest.json"
+    },
+    {
+      "hash": "sha256-iVBpy38O7I0ZN\/8aLvd8rZbhDsdaTSqr0JxAmjuEh48=",
+      "url": "_framework\/_bin\/BlazorUnicode.dll"
+    },
+    {
+      "hash": "sha256-QeyBSRhUtzRJySRlAe9BbT2+FtfVwjQNIsEyy4O9e2g=",
+      "url": "_framework\/_bin\/Microsoft.AspNetCore.Components.dll"
+    },
+    {
+      "hash": "sha256-cV6ToKOFYW7vUP+N\/G6N01848fkETOxNrokiTPmYmUQ=",
+      "url": "_framework\/_bin\/Microsoft.AspNetCore.Components.Web.dll"
+    },
+    {
+      "hash": "sha256-+J2yAcIBYkrwYU+NuM6DWGR7aOKtRCKyT0gJQKEW\/Xk=",
+      "url": "_framework\/_bin\/Microsoft.AspNetCore.Components.WebAssembly.dll"
+    },
+    {
+      "hash": "sha256-jzHgXWAvWMkKIGFjZoT84tbe72E+H7CvTr\/Dryh4QPs=",
+      "url": "_framework\/_bin\/Microsoft.Bcl.AsyncInterfaces.dll"
+    },
+    {
+      "hash": "sha256-ilDnhcTnaDEz9qgO76nIz51geEbKuWoqlbqe037OfVM=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.Configuration.Abstractions.dll"
+    },
+    {
+      "hash": "sha256-HaDJRSvYM0g7iHbGXeUM+mxuyTYkthr3iGXSvanSC5Y=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.Configuration.dll"
+    },
+    {
+      "hash": "sha256-rRBYv3NbNvRkprr8yIu7xLIwLD5PVNlUeW7qu3JmUuo=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.Configuration.Json.dll"
+    },
+    {
+      "hash": "sha256-+3rvp+MgUHLBfFoZWXtNuaTZTOXXZkbqI6nbn41jQqQ=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.DependencyInjection.Abstractions.dll"
+    },
+    {
+      "hash": "sha256-+ZmgyS0xkaenpVcHgX801saPTnIFrrG7PGunAyP7WPM=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.DependencyInjection.dll"
+    },
+    {
+      "hash": "sha256-2y4NfM0emFw45Et4SJibj9raMSnNLGlDnSv7IwSaIVI=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.Logging.Abstractions.dll"
+    },
+    {
+      "hash": "sha256-lorkraOJr1zVbFGJbp0APXp9h5SI3Ko4gE5XOLbcMoc=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.Logging.dll"
+    },
+    {
+      "hash": "sha256-Ou8j8ZNckf5ElrQk6q3HCp7VOG60UA2Ke7\/VftLiuck=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.Options.dll"
+    },
+    {
+      "hash": "sha256-NAg1NcoK7Zr6kb7o3NZFKqdgL2viWag0a5GimqyjmvY=",
+      "url": "_framework\/_bin\/Microsoft.Extensions.Primitives.dll"
+    },
+    {
+      "hash": "sha256-GFqkJmxwbf7WMrZ+MCRzaOADzSsVQ9tcMrMt19fsEeo=",
+      "url": "_framework\/_bin\/Microsoft.JSInterop.dll"
+    },
+    {
+      "hash": "sha256-UZa5CS19ZrbM6Csnl8CUIQucvmKMc4TughggTxKhx\/I=",
+      "url": "_framework\/_bin\/Microsoft.JSInterop.WebAssembly.dll"
+    },
+    {
+      "hash": "sha256-RVYBjLjoq4CB+UCHJVdwYYTuDvino8khjutRxCq7k48=",
+      "url": "_framework\/_bin\/mscorlib.dll"
+    },
+    {
+      "hash": "sha256-t4jD6h2O7SYBBwYXXFXHRYolJ+23rWeIQBvnhlTkNFo=",
+      "url": "_framework\/_bin\/System.Core.dll"
+    },
+    {
+      "hash": "sha256-rghB0bTlfAw30H+\/+zyHyj14DOWsx9G7hRXkwlu97+8=",
+      "url": "_framework\/_bin\/System.dll"
+    },
+    {
+      "hash": "sha256-u2w8nTiPwJ5+7u2v7SvK+t2sWGpQGgpne0ZGiTOfYX8=",
+      "url": "_framework\/_bin\/System.Net.Http.dll"
+    },
+    {
+      "hash": "sha256-PtF\/leZdQreMyJPyG76K\/rF+rj0DbRbFN++5Oy7QVFs=",
+      "url": "_framework\/_bin\/System.Net.Http.WebAssemblyHttpHandler.dll"
+    },
+    {
+      "hash": "sha256-Y3e5rkdepkb0OBES1uJDBzx0Mx0UnsDJ\/cHjlDnbSog=",
+      "url": "_framework\/_bin\/System.Runtime.CompilerServices.Unsafe.dll"
+    },
+    {
+      "hash": "sha256-4Kr7XIidIXyWOEwys+C8Nnt7BUsynYB5UXYHkX7T16E=",
+      "url": "_framework\/_bin\/System.Text.Encodings.Web.dll"
+    },
+    {
+      "hash": "sha256-5PVPvDYUt+jCLfE8F6rIHZ\/WzWuzPHVsQi+RpyCU6zY=",
+      "url": "_framework\/_bin\/System.Text.Json.dll"
+    },
+    {
+      "hash": "sha256-1TgJ5xv\/MK7ZuPAtF9I6uLhCTig3E682jSv3lh1AIWk=",
+      "url": "_framework\/_bin\/WebAssembly.Bindings.dll"
+    },
+    {
+      "hash": "sha256-mPoqx7XczFHBWk3gRNn0hc9ekG1OvkKY4XiKRY5Mj5U=",
+      "url": "_framework\/wasm\/dotnet.3.2.0.js"
+    },
+    {
+      "hash": "sha256-3S0qzYaBEKOBXarzVLNzNAFXlwJr6nI3lFlYUpQTPH8=",
+      "url": "_framework\/wasm\/dotnet.timezones.dat"
+    },
+    {
+      "hash": "sha256-UC\/3Rm1NkdNdlIrzYARo+dO\/HDlS5mhPxo0IQv7kma8=",
+      "url": "_framework\/wasm\/dotnet.wasm"
+    },
+    {
+      "hash": "sha256-oU0moF6IIkna1pMoSD6Ze63s6aHfjCtUflz7xDmENPg=",
+      "url": "_framework\/blazor.webassembly.js"
+    },
+    {
+      "hash": "sha256-H22CsTyRHc5MssxTVXrW\/5xngh7go735ezI4rxu8Anw=",
+      "url": "_framework\/blazor.boot.json"
+    }
+  ],
+  "version": "47DEQpj8"
+};
---
+++ readme.md
@@ -0,0 +1,37 @@
+## Blazor Unicode WASM + Progressive Web Application
+
+This is a PWA convergence of my C# code snippets built over the years to work with Unicode and Unicode fonts. The original code is based on several [LINQPad](https://www.linqpad.net/) C# snippets. This application uses [Microsoft's own (excellent) Cascadia Code font](https://github.com/microsoft/cascadia-code); for both the uesr interface and for the rendering of the code points.
+
+You can try this application from here:  [https://danielwcarey.github.io/BlazorUnicode](https://danielwcarey.github.io/BlazorUnicode)
+
+Some of the things I find myself doing frequently is:
+* Looking for the Unicode values for box elements.
+* Looking to see if there is something in the font I can use (name searches).
+* Given a page that has a character, just learning more about that (uni)code point.
+* Just browsing all the effort put into a font.
+
+
+Here are some useful links and tools:
+* [Microsoft Cascadia Code font](https://github.com/microsoft/cascadia-code)
+* [LINQPad](https://www.linqpad.net/)
+* [High-Logic FontCreator](https://www.high-logic.com/font-editor/fontcreator)
+* [High-Logic MainType](https://www.high-logic.com/font-manager/maintype)
+
+## Screenshots
+The Main Page
+![](docs/Image1.png)
+
+Browsing the Arrows
+![](docs/Image2.png)
+
+Browsing from the Code Point 2500
+![](docs/Image3.png)
+
+Example Search for 'angle'
+![](docs/Image4.png)
+
+Browsing Map Symbols
+![](docs/Image5.png)
+
+Browsing Emoticons
+![](docs/Image6.png)
