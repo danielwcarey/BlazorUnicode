59a23dce18a60a5787abfaee7fe42b4d9a5ef373
Daniel Carey <whawke8@hotmail.com>
2025-01-04 12:12:09 -0600

    Added publish (#4)
    Updated bootstrap and fonts
    
    Updating test dockerfile.wasm.
    Moved images for readme into docs folder.
    Code cleanup.

--- src/BlazorUnicode/Program.cs
+++ src/BlazorUnicode/Program.cs
@@ -3,19 +3,15 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<BlazorUnicode.App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddHttpClient("BlazorUnicode", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+var httpClient = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
+builder.Services.AddScoped(_ => httpClient);
 
-// Supply HttpClient instances that include access tokens when making requests to the server project
-builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorUnicode.ServerAPI"));
-
-//builder.Services.AddScoped<UnicodeCharacterDatabase>(service =>
-//    new UnicodeCharacterDatabase(
-//        service.GetRequiredService<HttpClient>()
-//    )
-//);
-builder.Services.AddScoped<UnicodeCharacterDatabase>();
+var unicodeCharacterDatabase = new UnicodeCharacterDatabase(httpClient);
+await unicodeCharacterDatabase.LoadDataAsync();
+builder.Services.AddScoped(_ => unicodeCharacterDatabase);
 
 await builder.Build().RunAsync();
