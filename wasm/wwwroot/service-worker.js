4b1b6acdbfb7e7ef1ddfba4d313541a08b5ff643
Daniel Carey <whawke8@hotmail.com>
2025-01-05 13:21:34 -0600

    Add search max result count and update assets (#6)
    - Introduced a new input field for "Max Result Count" in `Search.razor`, enhancing user control over search results.
    - Updated form elements with Bootstrap classes for improved styling and spacing.
    - Modified search logic to utilize the new `MaxResultCount` property.
    - Renamed and updated asset entries in `BlazorUnicode.staticwebassets.endpoints.json` to reflect new file names and sizes.
    - Deleted old WebAssembly files and transitioned to `BlazorUnicode.svhh2dfger.wasm`, indicating a rebuild of the application.
    - Updated `blazor.boot.json` and `service-worker-assets.js` to reference the latest assembly names and versions.
    - Overall improvements enhance the search feature and ensure the application uses the latest resources.

--- wasm/wwwroot/service-worker.js
+++ wasm/wwwroot/service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: Eb3y5cJh */
+/* Manifest version: tzJG63JN */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
